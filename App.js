b878ea0fdc39a1ee7ff479c09c68fa1e7af8822b
Puneet Arora <arorapuneet234@gmail.com>
2026-04-22 16:25:24 +0530

    Created top level components

--- App.js
+++ App.js
@@ -1,2 +1,80 @@
+import ReactDOM from "react-dom/client";
 import React from "react";
-import ReactDom from "react-dom";
+import logo from "./assets/prime-basket-logo.jpeg";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://prime-basket.developmentalphawizz.com/resources/themes/alpha_ecom/public/assets/images/primeLogo.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const ProductCard = () => {
+  return (
+    <div className="prod-card">
+      <img
+        className="prod-img"
+        src="https://m.media-amazon.com/images/I/71JGCn1z1TL._AC_UY436_FMwebp_QL65_.jpg"
+        alt="test"
+      />
+      <h3>Iphone 17 Pro</h3>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">
+        <h1>Search</h1>
+      </div>
+      <div className="prod-container">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
